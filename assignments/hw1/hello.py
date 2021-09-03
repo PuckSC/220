eace21fad838ed24773643ca1dfa5c1c4cb4cfe5
PuckSC <patrick7464@gmail.com>
2021-09-03 13:03:04 -0400

    HW1

--- assignments/hw1/hello.py
+++ assignments/hw1/hello.py
@@ -7,7 +7,9 @@ Problem: print hello, world! to the screen
 Certification of Authenticity:
 I certify that this assignment is entirely my own work."""
 
+
 def main():
     print("hello, world!")
+
 
 main()
